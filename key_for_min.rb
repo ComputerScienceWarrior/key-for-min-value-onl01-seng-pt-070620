ab8a7a182a80b1f9c1d6b4c34e6e24c25ac9c149
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-12 02:30:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   values = [] #holds all values
   smallest = 0 #hold
   i = 0
+  
   name_hash.collect do |key, val|
     values.push(val) #tested and works..is storing values
-    temp = values[0]; #set smallest to be the first value
-    
+    return values
   end
 end
