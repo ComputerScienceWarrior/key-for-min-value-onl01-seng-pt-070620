87c28b8a8d781987fd1ed0195083c3c9104bdf80
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-12 02:30:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,17 +8,6 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key, val|
     values.push(val) #tested and works..is storing values
     temp = values[0]; #set smallest to be the first value
-    if temp < values[i + 1]
-      #do nothing
-    elsif temp == values[i + 1]
-      #also, do nothing they're the same values
-    elsif temp > values[i + 1]
-      #swap the values!
-      smallest = values[i + 1]
-      values[i] = values[i + 1] 
-      values[i + 1] = temp
-      temp = values[i + 1]
-    end
-    i += 1
+    
   end
 end
