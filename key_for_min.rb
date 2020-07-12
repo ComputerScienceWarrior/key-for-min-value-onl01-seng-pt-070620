81fa8bdc65a493c61cab8476e2fe4b07d5ba5d46
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-12 02:38:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,15 +6,18 @@ def key_for_min_value(name_hash)
     return #returns nil
   end
   values = [] #holds all values
-  
+  smallest = nil
+  lowest_k = nil
   name_hash.collect do |key, val|
     values.push(val) #tested and works..is storing values
   end
   
-     if lowest_value == nil || v < lowest_value
-      lowest_value = v 
-      lowest_key = k 
+  name_hash.each do |key, val|
+    if smallest == nil || val < smallest
+      smallest = val
+      lowest_key = key
     end 
+  end 
   
-  return values
+  return key
 end
